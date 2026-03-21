41adcb26d174db973e9fddb029ac8f07ab8ab046
mohmohwin2010 <mohmohwin2010@gmail.com>
2026-03-21 22:57:22 +0630

    feat: added dynamic route, custom components, optimized image with NextJS Image

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,28 @@
-export default function MealsPage() {
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+export default async function MealsPage() {
+    // const meals = await getMeals
     return (
-        
-            <h1>Meals Page</h1>
+        <>
+         <header className={classes.header}>
+            <h1>
+                Delicious meals, created <span className={classes.highlight}>by  you</span>
+            </h1>
+            <p>
+                Choose you favourite recipe and cook it yourself. It is easy and fun!
+            </p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">
+                    Share Your Favourite Receipe
+                </Link>
+            </p>
+         </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]} />
+        </main>
+        </>
+           
            
         
     )
--- app/page.js
+++ app/page.js
@@ -1,27 +1,73 @@
 import Link from "next/link";
 
+import classes from './page.module.css';
+
 export default function Home() {
   return (
+
+    <>
+    <header className={classes.header}>
+      <div className={classes.slideshow}></div>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Taste & share food from all over the world.</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
+        </div>
+      </div>
+    </header>
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p>
-<Link href="/meals" style={{ display: 'block', textAlign: 'center', marginTop: '20px', color: 'white' }}>
-        Meals
-      </Link>
-      </p>
-      <p>
-<Link href="/meals/share" style={{ display: 'block', textAlign: 'center', marginTop: '10px', color: 'white' }}>
-        Share Meals
-      </Link>
-      </p>
-      <p>
- <Link href="/community" style={{ display: 'block', textAlign: 'center', marginTop: '10px', color: 'white' }}>
-        Community
-      </Link>
-      </p>
-     
+      <section className={classes.section}>
+        <h2>How it works</h2>
+        <p>
+          NextLevel Food is a platform for foodies to share their favorite recipes 
+          with the world. It&apos;s a place to discover new dishes, and to connect
+          with other food lovers.
+        </p>
+        <p>
+          NextLevel Food is a place to discover new dishes, and to connect with
+          other food lovers.
+        </p>
+      </section>
+      <section className={classes.section}>
+        <h2>Why NextLevel Food?</h2>
+        <p>
+          NextLevel Food is a platform for foodies t share their favorite recipes 
+          with the world. It&apos;s a place to discover new dishes, and to connect 
+          with other food lovers.
+        </p>
+        <p>
+          NextLevel Food is a place to discover new dishes, and to conect with
+          other food lovers.
+        </p>
+      </section>
     </main>
+    </>
+
+
+//     <main>
+//       <h1 style={{ color: 'white', textAlign: 'center' }}>
+//         Time to get started!
+//       </h1>
+//       <p>
+// <Link href="/meals" style={{ display: 'block', textAlign: 'center', marginTop: '20px', color: 'white' }}>
+//         Meals
+//       </Link>
+//       </p>
+//       <p>
+// <Link href="/meals/share" style={{ display: 'block', textAlign: 'center', marginTop: '10px', color: 'white' }}>
+//         Share Meals
+//       </Link>
+//       </p>
+//       <p>
+//  <Link href="/community" style={{ display: 'block', textAlign: 'center', marginTop: '10px', color: 'white' }}>
+//         Community
+//       </Link>
+//       </p>
+     
+//     </main>
   );
 }
