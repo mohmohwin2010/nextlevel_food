0d090b7bfe4fbd45dd30c70b1ebbc5bf29a422fe
mohmohwin2010 <mohmohwin2010@gmail.com>
2026-03-23 09:05:23 +0630

    fixed: display in detail image

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -2,27 +2,28 @@
 import Image from 'next/image';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
-export default function MealDetailsPage({params}){
-    const meal = getMeal(params.mealSlug);
-    console.log(meal)
+export default async function MealDetailsPage({params}){
+    const { mealSlug } = await params;
+    const meal =  getMeal(mealSlug);
 
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
     return(
         <>
         <header className={classes.header}>
             <div className={classes.image}>
-                <Image src="" alt="Meal image" fill />
+                <Image src={meal.image} alt={meal.title} fill />
             </div>
             <div className={classes.headerText}>
                 <h1>{meal.title}</h1>
                 <p className={classes.creator}>
-                    by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+                    by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
                 </p>
-                <p className={classes.summary}>SUMMARY</p>
+                <p className={classes.summary}>{meal.summary}</p>
             </div>
         </header>
         <main>
             <p className={classes.instructions} dangerouslySetInnerHTML={{
-                __html: '...',
+                __html: meal.instructions,
             }}>
 
             </p>
